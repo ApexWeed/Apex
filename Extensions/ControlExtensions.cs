0edf7754fc5deb61d1fdc8bba2677a956b9ea95e
apexweed <heli.freak.95@gmail.com>
2017-08-06 18:39:44 +1000

    Add control extensions.

---
+++ Extensions/ControlExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Apex.Extensions
+{
+    public static class ControlExtensions
+    {
+        public static void SplitWidth(this Control control)
+        {
+            if (control.Controls.Count == 0)
+                return;
+
+            var width = control.Width / control.Controls.Count;
+            for (var i = 0; i < control.Controls.Count; i++)
+            {
+                var child = control.Controls[i];
+                child.Width = width;
+                child.Left = width * i;
+
+                if (i == control.Controls.Count - 1)
+                    child.Width += control.Width % width;
+            }
+        }
+        public static void SplitHeight(this Control control)
+        {
+            if (control.Controls.Count == 0)
+                return;
+
+            var height = control.Height / control.Controls.Count;
+            for (var i = 0; i < control.Controls.Count; i++)
+            {
+                var child = control.Controls[i];
+                child.Height = height;
+                child.Top = height * i;
+
+                if (i == control.Controls.Count - 1)
+                    child.Height += control.Height % height;
+            }
+        }
+    }
+}
